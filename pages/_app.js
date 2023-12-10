990d4bdaae56b620621fd57185ce6e016ab442dc
mario-george <vortexspades@gmail.com>
2023-12-10 20:43:48 +0200

    AnimatePresence used while routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import Layout from "@/components/Layout";
-import "../styles/globals.css";
+import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import RoutingTransition from "@/components/RoutingTransition";
+import { AnimatePresence, motion } from "framer-motion";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  // console.log(router.route);
+  // console.log(router.pathname);
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <RoutingTransition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
