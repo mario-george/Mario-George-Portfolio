dfcb01fa6cb411bcd03eb323d6ea7fb5b08ce213
mario-george <vortexspades@gmail.com>
2023-12-11 15:11:45 +0200

    circles bg and animation particles and the Main Avatar

---
+++ components/MainAvatar.js
@@ -0,0 +1,22 @@
+// next image
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <div className='hidden xl:flex xl:max-w-none'>
+    <img
+      src={'/MainAvatar.jpg'}
+      width={700}
+      height={643}
+      alt=''
+      className='translate-z-0 w-full h-full'
+      style={{
+          mixBlendMode: "", // Apply the blend mode
+          backgroundColor: "transparent", // Set background color to transparent
+        }}
+    />
+  </div>
+  );
+};
+
+export default Avatar;
