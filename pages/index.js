c114e23c05e6ab449e73edbf08dc6cf46bdc8a44
mario-george <vortexspades@gmail.com>
2023-12-11 15:13:05 +0200

    the project button and the home page

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,80 @@
-import { motion } from "framer-motion";
-export default function HomePage() {
-  return (
-    <>
-      <motion.div
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 1, ease: "easeInOut" }}
-        className="z-30 mt-[20rem] "
-      >
-        Animated Content
-      </motion.div>
-      <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
-        Interactive Element
-      </motion.div>
-      <motion.div initial={{ scale: 0.5 }} animate={{ scale: 1 }}>
-        Element with Scaling
-      </motion.div>
-      <motion.div initial={{ x: 300 }} animate={{ x: 0 }}>
-        Element moving left (from right to left)
-      </motion.div>
+// next image
+import Image from "next/image";
 
-      <motion.div className="ml-[50%]" initial={{ x: -300 }} animate={{ x: 0 }}>
-        Element moving left
-      </motion.div>
-    </>
+// components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsButton";
+import MainAvatar from "../components/MainAvatar";
+
+// framer motion
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "../components/VariantsFramerMotion/VariantsObject";
+
+const Home = () => {
+  return (
+    <div className="bg-primary/60 h-full relative">
+      {/* image */}
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* background image */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* particles */}
+        <ParticlesContainer />
+        {/* avatar img */}
+        <motion.div
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-full h-full max-w-[700px] max-h-[643px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+        >
+          <MainAvatar />
+        </motion.div>
+      </div>
+
+      {/* text */}
+      <div className="w-full h-full relative bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* title */}
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Effective solutions <br /> for{" "}
+            <span className="text-accent">your business</span>
+          </motion.h1>
+          {/* subtitle */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            Creation of websites that increase sales, improve customer interaction and increase brand awareness.
+          </motion.p>
+          {/* btn */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
