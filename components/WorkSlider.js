7a9b52ac0496366984899debed4918387e7465bf
mario-george <vortexspades@gmail.com>
2023-12-12 23:16:32 +0200

    work slider

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,5 +1,4 @@
-
-import Link from 'next/link'
+import Link from "next/link";
 // work slider data
 export const workSlider = {
   slides: [
@@ -90,16 +89,14 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-
-                    <Link href={image.link} passHref legacyBehavior>
-
-                  <a
-                    className="relative rounded-lg overflow-hidden flex items-center justify-center group noopener noreferrer"
-                    key={index}
-                    target="_blank"
-                  >
-                    <div className="flex items-center justify-center relative overflow-hidden group">
-                      {/* image */}
+                  <Link href={image.link} passHref legacyBehavior>
+                    <a
+                      className="relative rounded-lg overflow-hidden flex items-center justify-center group noopener noreferrer"
+                      key={index}
+                      target="_blank"
+                    >
+                      <div className="flex items-center justify-center relative overflow-hidden group">
+                        {/* image */}
 
                         <Image
                           src={image.path}
@@ -108,25 +105,25 @@ const WorkSlider = () => {
                           className="max-w-[500px] max-h-[300px]"
                           alt=""
                         />
-                      {/* overlay gradient */}
-                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
-                      {/* title */}
-                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                          {/* title part 1 */}
-                          <div className="delay-100">Open</div>
-                          {/* title part 2 */}
-                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                            Project
-                          </div>
-                          {/* icon */}
-                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-                            <BsArrowRight />
+                        {/* overlay gradient */}
+                        <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                        {/* title */}
+                        <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                            {/* title part 1 */}
+                            <div className="delay-100">Open</div>
+                            {/* title part 2 */}
+                            <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                              Project
+                            </div>
+                            {/* icon */}
+                            <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                              <BsArrowRight />
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </a>
+                    </a>
                   </Link>
                 );
               })}
