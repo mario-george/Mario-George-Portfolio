28d035c5a0f2255ae7d051809438fa0d31fda3dd
mario-george <vortexspades@gmail.com>
2023-12-15 13:13:42 +0200

    make width and height dynamic and responsive

--- components/Buttons/DownloadCV.js
+++ components/Buttons/DownloadCV.js
@@ -2,8 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { HiArrowDown } from "react-icons/hi2";
 
-const DownloadCV = () => {
+const DownloadCV = ({mob}) => {
 
+let w1=185
+let h1=185
+
+let w2=170
+let h2=170
+if(mob){
+w1=120
+h1=120
+w2=130
+h2=130
+}
   const downloadCVHandler = () => {
 
     const linkDownloadPDF = document.createElement('a')
@@ -18,12 +29,12 @@ const DownloadCV = () => {
         className="mx-auto xl:mx-0 group cursor-pointer "
         onClick={downloadCVHandler}
       >
-        <div className="bg-circleStar bg-no-repeat bg-cover flex items-center justify-center h-[185px] w-[185px] relative">
+        <div className={`bg-circleStar bg-no-repeat bg-cover flex items-center justify-center h-[${h1}px] w-[${w1}px] ] relative`}>
           <Image
             src={"/download-cv-text.png"}
-            width={170}
-            height={170}
-            className="animate-spin-slow max-w-[170px] max-h-[170px]"
+            width={w2}
+            height={h2}
+            className={`animate-spin-slow max-w-[${w2}px] max-h-[${h2}px]`}
           />
           <HiArrowDown className="transition-all duration-300 group-hover:translate-y-3 transform text-4xl absolute " />
         </div>
