a61e9786719ef8405d22937cb9b3d92eb54d8bed
mario-george <vortexspades@gmail.com>
2024-01-11 18:41:50 +0200

    Add new projects to work slider

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,53 +1,74 @@
 import Link from "next/link";
 // work slider data
+import { FaCode, FaEye } from "react-icons/fa6";
+
 export const workSlider = {
   slides: [
     {
       images: [
         {
-          title: "Image gallery",
-          path: "/projects/imageGallery.png",
-          link: "https://image-gallery-orcin-nine.vercel.app/",
+          title: "Places Full Stack Website",
+          path: "/projects/placesProject.png",
+          demoLink: "https://places-project-front.vercel.app/",
+          githubLink: "https://github.com/mario-george/places-project-front",
         },
         {
-          title: "Pricing Cards",
-          path: "/projects/pricingCards.png",
-          link: "https://pricing-cards.vercel.app/",
-        },
-        {
-          title: "Product Modal",
-          path: "/projects/productModal.png",
-          link: "https://product-modal-tailwind-css.vercel.app/",
+          title: "Meetups App with Next",
+          path: "/projects/MeetupsNext.png",
+          demoLink: "https://meetups-next-red.vercel.app/",
+          githubLink: "https://github.com/mario-george/MeetupsNext.JS",
         },
         {
           title: "Ecommerce",
           path: "/projects/ecommerceCourses.png",
-          link: "https://ecommerce-courses-roan.vercel.app/",
+          demoLink: "https://ecommerce-courses-roan.vercel.app/",
+          githubLink: "https://github.com/mario-george/Ecommerce-Courses",
+        },
+        {
+          title: "Image gallery",
+          path: "/projects/imageGallery.png",
+          demoLink: "https://image-gallery-orcin-nine.vercel.app/",
+          githubLink: "https://github.com/mario-george/Image-Gallery",
         },
       ],
     },
     {
       images: [
         {
-          title: "React Meal App",
-          path: "/projects/reactMealApp.png",
-          link: "https://react-meal-app-nine.vercel.app/",
+          title: "Pricing Cards",
+          path: "/projects/pricingCards.png",
+          demoLink: "https://pricing-cards.vercel.app/",
+          githubLink: "https://github.com/mario-george/Pricing-Cards",
         },
         {
-          title: "Meetups App with Next",
-          path: "/projects/MeetupsNext.png",
-          link: "https://meetups-next-red.vercel.app/",
+          title: "Product Modal",
+          path: "/projects/productModal.png",
+          demoLink: "https://product-modal-tailwind-css.vercel.app/",
+          githubLink:
+            "https://github.com/mario-george/product-modal-TailwindCSS",
         },
-        {
-          title: "NARQA",
-          path: "/projects/narqa-logo.png",
-        link:"https://github.com/mario-george/NARS"
-        },
-    
+
         {
           title: "Email Subscribe",
           path: "/projects/emailSubscribe.png",
-          link: "https://email-subscribe-ruddy.vercel.app/",
+          demoLink: "https://email-subscribe-ruddy.vercel.app/",
+          githubLink: "https://github.com/mario-george/Email-Subscribe",
+        },
+        {
+          title: "React Meal App",
+          path: "/projects/reactMealApp.png",
+          demoLink: "https://react-meal-app-nine.vercel.app/",
+          githubLink: "https://github.com/mario-george/react-meal-app",
+        },
+      ],
+    },
+    {
+      images: [
+        {
+          title: "NARQA",
+          path: "/projects/narqa-logo.png",
+          demoLink: "https://github.com/mario-george/NARS",
+          githubLink: "https://github.com/mario-george/NARS",
         },
       ],
     },
@@ -86,42 +107,67 @@ const WorkSlider = () => {
             <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-                  <Link  key={index} href={image.link} passHref legacyBehavior>
-                    <a
-                      className="relative rounded-lg overflow-hidden flex items-center justify-center group noopener noreferrer"
-                      key={index}
-                      target="_blank"
+                  <div
+                    key={index}
+    
+                  >
+                    <div
+                      className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+          
                     >
                       <div className="flex items-center justify-center relative overflow-hidden group">
                         {/* image */}
-
                         <Image
                           src={image.path}
                           width={500}
                           height={300}
-                          // className="max-w-[500px] max-h-[300px]"
                           alt=""
                         />
                         {/* overlay gradient */}
-                        <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                        <div className="absolute inset-0 bg-gradient-to-r from-transparent to-gray-500 opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                         {/* title */}
                         <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                          <div className="flex items-center  text-[13px] tracking-[0.2em] space-x-12">
                             {/* title part 1 */}
-                            <div className="delay-100">Open</div>
+                            <Link
+                              key={index}
+                              href={image.githubLink}
+                              passHref
+                              legacyBehavior
+                            >
+                              <a
+                                className=" noopener noreferrer"
+                                key={index}
+                                target="_blank"
+                              >
+                                {" "}
+                                <div className=" text-4xl delay-100  duration-100 hover:scale-125 transition-all">
+                                  <FaCode />
+                                </div>
+                              </a>
+                            </Link>
                             {/* title part 2 */}
-                            <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
-                              Project
-                            </div>
-                            {/* icon */}
-                            <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
-                              <BsArrowRight />
-                            </div>
+                            <Link
+                              key={index}
+                              href={image.demoLink}
+                              passHref
+                              legacyBehavior
+                            >
+                              <a
+                                className="noopener noreferrer"
+                                key={index}
+                                target="_blank"
+                              >
+                                <div className="rounded-full text-4xl translate-y-[500%] group-hover:translate-y-0 transition-all delay-150 duration-100 hover:scale-125">
+                                  <FaEye />
+                                </div>
+                              </a>
+                            </Link>
                           </div>
                         </div>
                       </div>
-                    </a>
-                  </Link>
+                    </div>
+                  </div>
                 );
               })}
             </div>
