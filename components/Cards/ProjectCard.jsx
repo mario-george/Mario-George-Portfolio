fb48f9c9c61da78442cb6a5c8d110da3344a9e5d
mario-george <vortexspades@gmail.com>
2024-01-16 22:40:38 +0200

    Add motion animation to ProjectCard component

--- components/Cards/ProjectCard.jsx
+++ components/Cards/ProjectCard.jsx
@@ -21,7 +21,17 @@ const ProjectCard = ({ ind, title, demoLink, githubLink, path }) => {
   }, [controls, inView]);
 
   return (
-  <></>
+    <motion.div
+      ref={ref}
+      initial={{ opacity: 0, x: dir }}
+      animate={controls}
+      exit={{ opacity: 0, x: dir }}
+      transition={{ duration: 0.5 }}
+      className="bg-white p-4 shadow-md rounded-md "
+    >
+
+     
+    </motion.div>
   );
 };
 
