8ef229bba0d3634aaa7ad8b7b3ac5ac7fd2af5a6
mario-george <vortexspades@gmail.com>
2024-01-16 22:43:43 +0200

    Formatted with prettier

--- components/Cards/ProjectCard.jsx
+++ components/Cards/ProjectCard.jsx
@@ -10,7 +10,7 @@ import Typography from "@mui/material/Typography";
 import { CardActions } from "@mui/material";
 import Link from "next/link";
 import { FaCode, FaEye } from "react-icons/fa6";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
 export function ActionAreaCard({ path, title, demoLink, githubLink }) {
   return (
@@ -19,48 +19,38 @@ export function ActionAreaCard({ path, title, demoLink, githubLink }) {
         <CardMedia component="img" height="140" image={path} alt={title} />
 
         <div className="flex items-center justify-center tracking-[0.2em] space-x-12 group absolute inset-0 transition-all duration-300">
-        <div className="absolute inset-0 bg-gradient-to-r from-transparent to-gray-500 opacity-0 group-hover:opacity-10 duration-300 transition-all"/>
+          <div className="absolute inset-0 bg-gradient-to-r from-transparent to-gray-500 opacity-0 group-hover:opacity-10 duration-300 transition-all" />
           <Link href={githubLink} passHref legacyBehavior>
             <a className="noopener noreferrer" target="_blank">
               <div className="relative text-4xl delay-100 translate-y-[1000%] group-hover:translate-y-0 duration-300 hover:scale-125 transition-all">
-                
-              <div className="relative bg-black rounded-full w-[100%] h-[100%] px-6 py-6 z-1"/>
+                <div className="relative bg-black rounded-full w-[100%] h-[100%] px-6 py-6 z-1" />
                 <div className="absolute inset-0 transform translate-x-[6px] translate-y-[6px] z-30">
-
-
-                <FaCode className="text-white" />
+                  <FaCode className="text-white" />
                 </div>
-
-
-
               </div>
             </a>
           </Link>
           <Link href={demoLink} passHref legacyBehavior>
             <a className="noopener noreferrer" target="_blank">
               <div className="rounded-full text-4xl translate-y-[1000%] group-hover:translate-y-0 transition-all delay-150 duration-300 hover:scale-125">
-                
-              <div className="relative bg-black rounded-full w-[100%] h-[100%] px-6 py-6 z-1"/>
+                <div className="relative bg-black rounded-full w-[100%] h-[100%] px-6 py-6 z-1" />
                 <div className="absolute inset-0 transform translate-x-[6px] translate-y-[6px] z-30">
-
-
-                <FaEye className="text-white"/>
+                  <FaEye className="text-white" />
                 </div>
-
               </div>
             </a>
           </Link>
-      </div>
         </div>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            description
-          </Typography>
-        </CardContent>
-        <CardActions>
+      </div>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          description
+        </Typography>
+      </CardContent>
+      <CardActions>
         <Button size="small">Frontend Repo</Button>
         <Button size="small">Backend Repo</Button>
         <Button size="small">Demo</Button>
@@ -92,7 +82,6 @@ const ProjectCard = ({ ind, title, demoLink, githubLink, path }) => {
       transition={{ duration: 0.5 }}
       className="bg-white p-4 shadow-md rounded-md "
     >
-
       <ActionAreaCard
         title={title}
         path={path}
