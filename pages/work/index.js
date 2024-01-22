5396dda9921acc241ddb4522cb41f4a860a173f3
mario-george <vortexspades@gmail.com>
2024-01-22 11:39:38 +0200

    update styling

--- pages/work/index.js
+++ pages/work/index.js
@@ -16,7 +16,7 @@ const Work = () => {
           <div className="flex flex-col xl:flex-row gap-x-8 mx-auto justify-center overflow-y-hidden">
             {/* Text */}
             <div
-              className="overflow-y-hidden text-center justify-center flex xl:w-[30vw] mx-auto  flex-col lg:text-left mb-4 
+              className="overflow-y-hidden text-center justify-center flex xl:w-[30vw] mx-auto  flex-col lg:text-left  
           xl:mb-0 "
             >
               <motion.h2
@@ -24,7 +24,7 @@ const Work = () => {
                 initial="hidden"
                 animate="show"
                 exit="hidden"
-                className="h2 xl:mt-12"
+                className="h2 xl:mt-12 text-center"
               >
                 My Projects<span className="text-accent">.</span>
               </motion.h2>
@@ -33,32 +33,34 @@ const Work = () => {
                 initial="hidden"
                 animate="show"
                 exit="hidden"
-                className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+                className=" max-w-[400px] mx-auto text-center"
               >
                 a captivating showcase of my passion for crafting exceptional
                 websites.From responsive designs to seamless user interfaces.
                 Whether you seek a stunning portfolio, an intuitive e-commerce
                 platform, or a dynamic corporate site, I integrate imagination,
                 practicality, and originality to ensure a blend of creativity
-                and functionality.
+                and functionality. I have multiple skills with different
+                programming languages making me a flexible web developer also
+                have the ability to create Mobile Apps Using React Native.
               </motion.p>
             </div>
             {/* slider */}
-            <motion.div
+            {/* will provide a way to change the order of the elements  */}
+            {/* <motion.div
               variants={fadeIn("down", 0.6)}
               initial="hidden"
               animate="show"
               exit="hidden"
               className="w-full xl:max-w-[65%]"
             >
-              {/* will provide a way to change the order of the elements  */}
-              {/* <WorkSlider /> */}
-            </motion.div>
+              <WorkSlider />
+            </motion.div> */}
           </div>
         </div>
         <Bulb />
       </div>
-      <div className="bg-primary/30 py-20 overflow-hidden ">
+      <div className="bg-primary/30 pb-20 overflow-hidden ">
         <div className="container mx-auto grid grid-cols-1  gap-4 ">
           {ProjectData.slides.map((image) => {
             return image.images.map((project, i) => {
@@ -81,7 +83,9 @@ const Work = () => {
             title={"React Native Guess Number"}
             path={"/projects/videos/react-native-guess-number.mp4"}
             demoLink={false}
-            githubLink={"https://github.com/mario-george/react-native-guess-number"}
+            githubLink={
+              "https://github.com/mario-george/react-native-guess-number"
+            }
             video={true}
           />{" "}
           <ProjectCard
@@ -90,7 +94,9 @@ const Work = () => {
             title={"React Native Goal Tracker"}
             path={"/projects/videos/react-native-goal-tracker.mp4"}
             demoLink={false}
-            githubLink={"https://github.com/mario-george/react-native-goals-tracker"}
+            githubLink={
+              "https://github.com/mario-george/react-native-goals-tracker"
+            }
             video={true}
           />
         </div>
